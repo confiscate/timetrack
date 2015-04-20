d629f2cf999996ab66c8c87867106ea7964a25ee
Henry <confiscate@gmail.com>
2015-04-20 00:11:06 -0700

    listview

--- app/src/main/java/com/example/henry/timetrack/MainActivity.java
+++ app/src/main/java/com/example/henry/timetrack/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ToggleButton;
+
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.cognito.*;
 import com.amazonaws.regions.Regions;
@@ -17,38 +19,54 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE = "com.example.henry.MESSAGE";
+    public CognitoCachingCredentialsProvider credentialsProvider;
+    public CognitoSyncManager syncClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+        //authenticateAWS();
+        //syncAWS()
+
+        setContentView(R.layout.activity_main);
+
+        fillHoursLog();
+    }
+
+    private void authenticateAWS() {
+        credentialsProvider = new CognitoCachingCredentialsProvider(
                 this, // Context
                 "", // Identity Pool ID
                 null // Region
         );
 
         // Initialize the Cognito Sync client
-        CognitoSyncManager syncClient = new CognitoSyncManager(
+        syncClient = new CognitoSyncManager(
                 this,
                 Regions.US_EAST_1, // Region
                 credentialsProvider);
+    }
 
+    private void syncAWS() {
         // Create a record in a dataset and synchronize with the server
         Dataset dataset = syncClient.openOrCreateDataset("lemontea");
         dataset.put("vita", "hentest2");
 
-        final Intent intent = new Intent(this, DisplayMessageActivity.class);
         dataset.synchronize(new DefaultSyncCallback() {
             @Override
             public void onSuccess(Dataset dataset, List newRecords) {
-                String message = dataset.get("vita");
-                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
             }
         });
+    }
 
-        setContentView(R.layout.activity_main);
+    private void fillHoursLog() {
+        final ListView hourslog = (ListView) findViewById(R.id.hourslog);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
     }
 
 
@@ -63,20 +81,22 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.action_search:
-                //openSearch();
-                return true;
             case R.id.action_settings:
-                //openSettings();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void onNotificationsToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+
+        }
+    }
+
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
+        String message = "hmm";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
